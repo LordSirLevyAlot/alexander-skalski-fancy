593a3d7a3bbd9959bce9e0b2a5fa801fb7a7468b
LordSirLevyAlot <62107487+LordSirLevyAlot@users.noreply.github.com>
2020-03-14 11:08:09 +0100

    Dropdown to navbar. Added JS to change title.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let aboutTitle = document.querySelector(".aboutTitle");
+let navItems = document.getElementsByClassName("navItem");
+
+navItems[1].onmouseenter = function(){
+    aboutTitle.innerHTML = 'About <i class="fas fa-angle-double-up"></i>';
+};
+
+navItems[1].onmouseleave = function(){
+    aboutTitle.innerHTML = 'About <i class="fas fa-angle-double-down"></i>';
+};
