730c2fa134fce001d4fff88ed49d96502a8996a1
LordSirLevyAlot <62107487+LordSirLevyAlot@users.noreply.github.com>
2020-03-28 12:22:08 +0100

    Added Batman-easteregg, section animations

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
-//window.addEventListener("scroll", handleScrolling);
+document.addEventListener('wheel', handleScrolling);
+window.addEventListener('resize scroll', handleScrolling);
+//window.onerror = window.prompt("opsie whoopsie");
+
+function handleScrolling(){
+    let höjd = document.body.scrollHeight;
+    let höjdAvSkärm = (window.innerHeight || document.documentElement.clientWidth);
+    /*let sak = document.getElementById("försökskanin");
+    let information = sak.getBoundingClientRect();*/
+    //console.log(höjd +"   " +höjdAvSkärm + "   ");
+
+
+
+let saker = document.querySelectorAll("section");
+
+for (let index = 0; index < saker.length; index++) {
+
+    let buff = saker[index].getBoundingClientRect();
+
+    if((buff.top + buff.height*0.5) >= 0 && buff.bottom <= (höjdAvSkärm+ buff.height*0.5))
+    {
+        saker[index].style.opacity = "1";
+    }else
+    {
+        saker[index].style.opacity = "0";
+    }
+    
+}
+
+
+}
+
+
 //let scrollat = window.scrollY;
