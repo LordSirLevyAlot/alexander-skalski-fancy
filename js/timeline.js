fc250f71f09b322da79c9dafc12fe2827597dce4
LordSirLevyAlot <62107487+LordSirLevyAlot@users.noreply.github.com>
2020-03-29 21:06:19 +0200

    Continued work on the timeline

--- js/timeline.js
+++ js/timeline.js
@@ -2,6 +2,7 @@ let startKnapp = document.getElementById("knapp");
 let navbar = document.querySelector("nav");
 let header = document.querySelector("header");
 let knappHållare = document.getElementById("knappHållare");
+let footer = document.querySelector("footer");
 startKnapp.addEventListener('click', påbörjaTimeline);
 
 let slides = document.getElementsByClassName("slide");
@@ -13,36 +14,45 @@ function påbörjaTimeline(){/*
     console.log(window.innerHeight);*/
 }
 
-let counter = 0;
+let counter = 1;
 function nästa(){
-    counter++;
+
+    if(counter == slides.length)
+        {return;}
     slides[counter].style.top = "0";
     slides[counter].style.zIndex = counter;
     slides[counter-1].style.opacity = "0";
+    if(counter == slides.length-1)
+    {return;}
+    counter++;
+    slides[counter].style.zIndex = counter;
 }
 function bakåt(){
+    if(counter==1)
+        {return;}
+
     slides[counter-1].style.opacity = "1";
     slides[counter].style.top = "100vh";
     counter--;
 }
+let blah = 0;
 
-
-let blah=0;
-function hejsvejs(){
-    console.log(document.body.scrollY);
-    blah++;
-    if(blah%10 == 0){
-        nästa();
-    }
-}
 
 window.addEventListener('wheel', event => {
     const delta = Math.sign(event.deltaY);
     console.info(delta);
     blah+=delta;
-    //slides[counter].style.top = blah + "vh";
-    if(blah%10 == 0){
+    console.log(blah*10 + "vh");
+    console.log(counter);
 
+    if(footer.getBoundingClientRect().top != 0){
+        console.log(footer.getBoundingClientRect().top + "ff");
+        SmoothTransition();
+    }
+
+
+    if(blah%5 == 0){
+        blah = 0;
         if(delta>0){
             nästa();
         }else{
@@ -51,3 +61,10 @@ window.addEventListener('wheel', event => {
 
     }
 });
+
+function SmoothTransition(){
+    try{
+        slides[counter].style.top = 100-blah*10 + "vh";
+    }
+    catch{}
+}
