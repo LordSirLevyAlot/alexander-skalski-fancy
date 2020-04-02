0c018729c120f56dc208d8ef1ad04d4e6fa72134
LordSirLevyAlot <62107487+LordSirLevyAlot@users.noreply.github.com>
2020-04-02 16:30:58 +0200

    DId some work on the timeline. Pondering over why stuff doesn't work

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 document.addEventListener('wheel', handleScrolling);
 window.addEventListener('resize scroll', handleScrolling);
-//window.onerror = window.prompt("opsie whoopsie");
+
+let senasteTouch = 1000;
+document.body.addEventListener('touchmove', handleScrolling);
+
+
 
 /*
 let width = window.width;
@@ -20,6 +24,7 @@ navbar.addEventListener('click', () =>{
     }
 })*/
     
+//Skulle vilja targeta alla element i första raden av gridet
 let saker = document.querySelectorAll("section");
 window.addEventListener('load', ()=>{
     saker[0].style.opacity = "1";
@@ -28,30 +33,20 @@ window.addEventListener('load', ()=>{
 function handleScrolling(){
     let höjd = document.body.scrollHeight;
     let höjdAvSkärm = (window.innerHeight || document.documentElement.clientWidth);
-    /*let sak = document.getElementById("försökskanin");
-    let information = sak.getBoundingClientRect();*/
-    //console.log(höjd +"   " +höjdAvSkärm + "   ");
 
+    for (let index = 0; index < saker.length; index++) {
 
-
-
-
-for (let index = 0; index < saker.length; index++) {
-
-    let buff = saker[index].getBoundingClientRect();
-
-    if((buff.top + buff.height*0.5) >= 0 && buff.bottom <= (höjdAvSkärm+ buff.height*0.5))
-    {
-        saker[index].style.opacity = "1";
-    }else
-    {
-        saker[index].style.opacity = "0";
+        let infoOmElementet = saker[index].getBoundingClientRect();
+        if((infoOmElementet.top + infoOmElementet.height*0.5) >= 0 && infoOmElementet.bottom <= (höjdAvSkärm + infoOmElementet.height*0.5))
+        {
+            saker[index].style.opacity = "1";
+        }else
+        {
+            saker[index].style.opacity = "0";
+        }
     }
-    
-}
 
 
 }
-
 
 //let scrollat = window.scrollY;
